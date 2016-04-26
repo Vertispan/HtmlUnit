e07cee830988bb386c6ab6080e7e81ccba2ecf6a
asashour <asashour@5f5364db-9458-4db8-a492-e30667be6df6>
2016-04-26 19:02:11 +0000

    js: uncomment SomeHostTest
    git-svn-id: http://svn.code.sf.net/p/htmlunit/code/trunk@12493 5f5364db-9458-4db8-a492-e30667be6df6

--- js/src/test/java/jdk/nashorn/SomeHost.java
+++ js/src/test/java/jdk/nashorn/SomeHost.java
@@ -10,40 +10,40 @@
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * version 2 for more details (http://www.gnu.org/licenses/).
  */
-//package jdk.nashorn;
-//
-//import java.lang.invoke.MethodHandle;
-//import java.lang.invoke.MethodHandles;
-//import java.lang.invoke.MethodType;
-//
-//import jdk.internal.dynalink.CallSiteDescriptor;
-//import jdk.internal.dynalink.linker.GuardedInvocation;
-//import jdk.internal.dynalink.linker.LinkRequest;
-//import jdk.nashorn.internal.runtime.ScriptObject;
-//
-//public class SomeHost extends ScriptObject {
-//
-//    public String aMethod() {
-//        return "in aMethod()";
-//    }
-//
-//    @Override
-//    protected GuardedInvocation findGetMethod(CallSiteDescriptor desc, LinkRequest request, String operator) {
-//        return new GuardedInvocation(virtualHandle("aMethod", String.class), (MethodHandle) null);
-//    }
-//
-//    private static MethodHandle virtualHandle(String name, Class<?> rtype, Class<?>... ptypes) {
-//        try {
-//            return MethodHandles.lookup().findVirtual(SomeHost.class, name, MethodType.methodType(rtype, ptypes));
-//        }
-//        catch (final ReflectiveOperationException e) {
-//            throw new IllegalStateException(e);
-//        }
-//    }
-//
-//    @Override
-//    public String getClassName() {
-//        return getClass().getSimpleName();
-//    }
-//
-//}
+package jdk.nashorn;
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
+import jdk.internal.dynalink.CallSiteDescriptor;
+import jdk.internal.dynalink.linker.GuardedInvocation;
+import jdk.internal.dynalink.linker.LinkRequest;
+import jdk.nashorn.internal.runtime.ScriptObject;
+
+public class SomeHost extends ScriptObject {
+
+    public String aMethod() {
+        return "in aMethod()";
+    }
+
+    @Override
+    protected GuardedInvocation findGetMethod(CallSiteDescriptor desc, LinkRequest request, String operator) {
+        return new GuardedInvocation(virtualHandle("aMethod", String.class), (MethodHandle) null);
+    }
+
+    private static MethodHandle virtualHandle(String name, Class<?> rtype, Class<?>... ptypes) {
+        try {
+            return MethodHandles.lookup().findVirtual(SomeHost.class, name, MethodType.methodType(rtype, ptypes));
+        }
+        catch (final ReflectiveOperationException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public String getClassName() {
+        return getClass().getSimpleName();
+    }
+
+}
